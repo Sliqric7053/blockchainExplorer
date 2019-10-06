0c6c0f7350153ef4df2e07f9ab54382327a7b8df
Richard Sithole <richardsit@gmail.com>
2019-10-06 05:44:31 +0200

    Bind block detail data to template

--- blockchaininfo/apps/blockchaininfo/src/app/components/dialog/dialog.component.ts
+++ blockchaininfo/apps/blockchaininfo/src/app/components/dialog/dialog.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit, Input, Optional, Inject } from '@angular/core';
-import { Block, MovieDialogBoxOptions } from '../../blocks/models/blocks.model';
+import { Component, OnInit, Optional, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { BlocksService } from '../../blocks/blocks.service';
+
+import { BlockDetailsResults } from '@blockchaininfo/api-interfaces';
+
 import { Observable } from 'rxjs';
+
+import { BlocksService } from '../../blocks/blocks.service';
 
 @Component({
   selector: 'blockchaininfo-dialog',
@@ -10,24 +13,36 @@ import { Observable } from 'rxjs';
   styleUrls: ['./dialog.component.scss']
 })
 export class DialogComponent implements OnInit {
-  dataSource$: Observable<Block>;
-  displayedColumns: string[] = ['Block hash', 'Block time', 'Block height'];
+  dataSource$: Observable<BlockDetailsResults>;
+  block: BlockDetailsResults;
   hash: string;
+  blogTotal = 0;
 
   constructor(
     public dialogRef: MatDialogRef<DialogComponent>,
-    private readonly blockService: BlocksService,
+    private blockService: BlocksService,
     /* prevent error if no data is passed */
-    @Optional() @Inject(MAT_DIALOG_DATA) public data: MovieDialogBoxOptions
+    @Optional() @Inject(MAT_DIALOG_DATA) public data
   ) {}
 
   ngOnInit() {
     this.hash = this.blockService.getHash();
-
     this.dataSource$ = this.blockService.getBlockDetails(this.hash);
+    this.processDetails();
   }
 
   closeDialog() {
     this.dialogRef.close();
+  }
+
+  processDetails() {
+    this.dataSource$.subscribe(details => {
+      this.block = details;
+      details.details.tx.map(transaction => {
+        transaction.out.map(outputItem => {
+          this.blogTotal += Number(outputItem.value);
+        });
+      });
+    });
   }
 }
